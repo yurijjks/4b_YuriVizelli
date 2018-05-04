883067306d07b0b657f4e226b748d52a8d46e7ae
04018523262 <yurijjks@gmail.com>
2018-05-04 14:50:11 -0400

    hibernate

--- 4b_yuri/src/br/edu/ifro/MenuPrincipalController.java
+++ 4b_yuri/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,11 +5,13 @@
  */
 package br.edu.ifro;
 
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -20,16 +22,30 @@ public class MenuPrincipalController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
+    private JFXTextField txtnum1;
+    @FXML
+    private JFXTextField txtnum2;
+    @FXML
+    private JFXTextField txtresult;
+    @FXML
+    private Button btnSoma;
+    
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+      
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void somar(ActionEvent event) {
+    Double num1 = Double.parseDouble(txtnum1.getText());
+    Double num2 = Double.parseDouble(txtnum2.getText());
+    Double result = num1+num2;
+    txtresult.setText(result.toString());
+    }
     
 }
